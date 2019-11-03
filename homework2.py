8f6b577d637028d99060ba1c65de1404d409d885
emayfield <9727647+emayfield@users.noreply.github.com>
2019-11-03 08:39:05 -0500

    Initial commit.

---
+++ homework2.py
@@ -0,0 +1,30 @@
+
+def q1_yelp():
+    pass
+
+def q2_yelp():
+    pass
+
+def q3_yelp():
+    pass
+
+# To import this function you will need to install the lxml library using Conda.
+from wiki_api import page_text
+
+def get_featured_biographies():
+    pass
+
+def get_first_paragraph(page):
+    pass
+
+def get_pronouns(text):
+    pass
+
+def additional_analysis():
+    pass
+
+def export_dataset(df):
+    pass
+
+if __name__ == "__main__":
+    pass
