a20a2ac3a8921bd9ca3085f64eba48a1fecb247a
zhoujuliana <56312807+zhoujuliana@users.noreply.github.com>
2019-11-22 23:04:55 -0500

    Update homework2.py
    Part 2, Question 1

--- homework2.py
+++ homework2.py
@@ -20,40 +20,44 @@ def q1_yelp():
 def q2_yelp():
     pass
 
+def q3_yelp():
+    pass
+
 # To import this function you will need to install the lxml library using Conda.
-from wiki_api.py import page_text
+from wiki_api import page_text
 
 # Pulling 
 if __name__ == '__main__':
     wiki_html = page_text("Wikipedia:Featured articles", "html")
     wiki_text = page_text("Wikipedia:Featured articles", "text")
     wiki_list = page_text("Wikipedia:Featured articles", "list")
-    
-    # Check
+
     print(wiki_list[0:500])
 
 # Part 2, Question 1
 def get_featured_biographies():
+    print(f"There are {len(wiki_list)} items in the object wiki_list.")
     final = []
     boolean = False
-
+    title = []
     for title in titles:
         if ('[edit]' in title) and ('Autobiographies' in title):
             boolean = False 
             continue 
-        elif ('[edit]' in title) and ('biographies' in title) or ('Biographies' in title):
+        elif ('[edit]' in title) and (('biographies' in title) or ('Biographies' in title)):
             boolean = True 
             continue 
         elif ('[edit]' in title) and ('biographies' not in title):
             boolean = False 
-        
+            
         if boolean:
             final = final + [title]
         else:
             continue 
-        print(final)
+            
+    final
 
-get_featured_biographies()
+    get_featured_biographies()
 
 def get_first_paragraph(page):
     pass
